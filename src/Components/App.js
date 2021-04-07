6c7a88baa54004a3bd87c882357b15968c07237a
Matt Umland <mattumland@gmail.com>
2021-04-07 12:26:39 -0400

    Add Form boilerplate

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ResGrid from './ResGrid';
+import Form from './Form';
 
 class App extends Component {
   constructor() {
@@ -21,7 +22,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form />
         </div>
         <div className='resy-container'>
           <ResGrid
---
+++ src/Components/Form.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state={
+      name:'',
+      date:'',
+      time:'',
+      number:''
+    }
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    return(
+      <form>
+        <input
+          type='text'
+          placeholder='Name'
+          name='name'
+          value={this.state.name}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <input
+          type='text'
+          placeholder='Date (mm/dd)'
+          name='date'
+          value={this.state.date}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <input
+          type='text'
+          placeholder='Time'
+          name='time'
+          value={this.state.time}
+          onChange={event => this.handleChange(event)}
+        />
+
+
+        <input
+          type='text'
+          placeholder='Number of guests'
+          name='number'
+          value={this.state.number}
+          onChange={event => this.handleChange(event)}
+        />
+
+        <button>Make Reservation</button>
+      </form>
+    )
+  }
+}
+
+export default Form;
